9e9c423a6f75f141493426d7cbcaf80ee2d0ff89
Julia Mayumi <julia.mayumi.costa@escola.pr.gov.br>
2024-04-12 08:52:43 -0300

    Estilizando o contador

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+//const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
@@ -50,7 +50,7 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-comecaCronometro();
+//comecaCronometro();
 
 
 let nomeUsuario = "";
